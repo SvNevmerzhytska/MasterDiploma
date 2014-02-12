625029b5f78681ba143d4ce6213c930412e361bf
SvNevmerzhytska <svitlana.nevmerzhytska@gmail.com>
2014-02-12 22:25:05 +0200

    Add Arc as datatype

--- diploma_workspace/SolvingMMkCPPwithDC/src/edu/kpi/master/datatypes/Arc.java
+++ diploma_workspace/SolvingMMkCPPwithDC/src/edu/kpi/master/datatypes/Arc.java
@@ -6,7 +6,39 @@ public class Arc {
 	private int transitCost;
 	private int demand;
 	private boolean includeToRoute;
+	private int priority;
 	
+	
+	public int getServiceCost() {
+		return serviceCost;
+	}
+	public void setServiceCost(int serviceCost) {
+		this.serviceCost = serviceCost;
+	}
+	public int getTransitCost() {
+		return transitCost;
+	}
+	public void setTransitCost(int transitCost) {
+		this.transitCost = transitCost;
+	}
+	public int getDemand() {
+		return demand;
+	}
+	public void setDemand(int demand) {
+		this.demand = demand;
+	}
+	public boolean isIncludeToRoute() {
+		return includeToRoute;
+	}
+	public void setIncludeToRoute(boolean includeToRoute) {
+		this.includeToRoute = includeToRoute;
+	}
+	public int getPriority() {
+		return priority;
+	}
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
 	
 
 }
